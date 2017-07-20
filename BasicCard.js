7069d0baa31d823aa1111ec38d86b13202c35a09
Jose Rivas <josevzla@android-6f6064df264c8670.local.valenciacc.edu>
2017-07-20 18:12:37 -0400

    hw9

---
+++ BasicCard.js
@@ -0,0 +1,10 @@
+var firstNba = new BasicCard(
+    "Who was the first NBA champion?", "Boston Celtics");
+
+
+console.log(firstNba.front); 
+
+console.log(firstNba.back); 
+
+
+module.exports = BasicCard;
---
+++ ClozeCard.js
@@ -0,0 +1,16 @@
+var firstPresidentCloze = new ClozeCard(
+    "Bost Celtics is the fist champion in the NBA.", "Boston Celtics");
+
+
+console.log(firstNbaCloze.cloze); 
+
+
+console.log(firstNbaCloze.partial); ""
+
+
+console.log(firstNbaCloze.fullText): ""
+
+
+var brokenCloze = new ClozeCard("Try Again", "Airball");
+
+module.exports = ClozeCard;
